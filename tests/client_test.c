ae722a626beb54e5e1f78a2b55bb52999a932dc2
veparkh <veparkhomenko@stud.etu.ru>
2023-08-07 19:24:26 +0300

    client struct Request added

--- tests/client_test.c
+++ tests/client_test.c
@@ -8,17 +8,25 @@ char msg_c[200];
 
 thread_t *tp = NULL;
 bool isConnectionEnabled = false;
-err_t write_data_c(struct netconn *conn){
+err_t write_data_c(struct netconn *conn,char *data){
     // При записи просто отправляем указатель на буфер и количество байт. Последний аргумент - флаг для записи. Но я пока про них ничего не понимаю
-	if(strlen(msg_c)!=0)
+	if(strlen(data)!=0)
 	{
-		err_t err = netconn_write(conn, msg_c, strlen(msg_c), NETCONN_NOCOPY);
+		err_t err = netconn_write(conn, data, strlen(data), NETCONN_NOCOPY);
 		strcpy(msg_c, "");
 		return err;
 	}
 	return netconn_write(conn, "hi there!", strlen("hi there!"), NETCONN_NOCOPY);
 }
+enum REQUEST_TYPE {
+	READ,
+	WRITE,
+};
 
+typedef struct  {   // Structure declaration
+  int TYPE;           // Member (int variable)
+  char myLetter[30];       // Member (char variable)
+} Request;
 err_t read_data_c(struct netconn *conn){
     // При чтении немного сложнее
     struct netbuf *inbuf;
@@ -28,7 +36,6 @@ err_t read_data_c(struct netconn *conn){
     // Ждем что нам напишут. Функция блокирующая. В inbuf лежит информация кто и что написал
 	netconn_set_recvtimeout(conn,6000);
     err_t err_recv = netconn_recv(conn, &inbuf);
-    dbgprintf("recv err:%d\n\r",err_recv);
    if (err_recv ==0)
    {
 	   // Память мы сами не выделяем, все делает lwip. Просто подсовываем известное имя, чтобы оно начало указывать на начало данных. Заодно узнаем количество байт
@@ -42,14 +49,15 @@ err_t read_data_c(struct netconn *conn){
     return err_recv;
 }
 
-static THD_FUNCTION(tcp_client, p) {
-  (void)p;
+
+
+static THD_FUNCTION(tcp_client, req) {
+  Request *request = req;
   chRegSetThreadName("client_thread");
   err_t err_connect;
 // Поленился нормально прокинуть сюда адрес, поэтому определяю его заново. Он должен быть такй же как и в мейне
   struct ip4_addr server_ip;
   IP4_ADDR(&server_ip, 192, 168, 1, 120);
-
     struct netconn *conn = netconn_new(NETCONN_TCP);
     if(conn==NULL)
     {
@@ -64,25 +72,33 @@ static THD_FUNCTION(tcp_client, p) {
       netconn_delete(conn);
       chThdExit(err_connect);
     }
-    chThdSleepMilliseconds(1000);
-    // Индикация что кто-то подключился
-    palToggleLine(LINE_LED1);
+    err_t error;
+    switch(request->TYPE){
+    case	READ:{
+    	error = read_data_c(conn);
+    	break;
+    	}
+    case WRITE:{
+    	error = write_data_c(conn,request->myLetter);
+    	break;
+    	}
 
-    // Прочитаем что-нибудь
-    err_t err_recv = read_data_c(conn);
+    default:{
+    	error = -100;
+    	}
+    }
     netconn_close(conn);
     netconn_delete(conn);
-    chThdExit(err_recv);
+    chThdExit(error);
+
+
 }
 
 
-
-msg_t getData(void){
-	tp = chThdCreateFromHeap(NULL, THD_WORKING_AREA_SIZE(1024),"tcp", NORMALPRIO, tcp_client, NULL);
+msg_t getData(Request request){
+	tp = chThdCreateFromHeap(NULL, THD_WORKING_AREA_SIZE(1024),"tcp", NORMALPRIO, tcp_client, &request);
 	return chThdWait(tp);
-
 }
-
 
 void up_callback(void *p)
 {
@@ -126,7 +142,8 @@ void client_test(void){
 
     while (true) {
     	if (isConnectionEnabled){
-        	msg_t err = getData();
+    		Request request = {WRITE,"Hi from client\r\n"};
+        	msg_t err = getData(request);
         	if(err==ERR_OK){
         		dbgprintf(msg_c);
         	}else{
